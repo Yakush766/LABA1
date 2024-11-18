c1eb7eb2a1bc1243ee29c60b3fd66f4862a8ce58
Yakush766 <anton.yakushenko72@gmail.com>
2024-11-19 01:00:10 +0300

    Нашёл ошибку в программе

--- программа.py
+++ программа.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(1, 100)  # Загаданное число
 print("Угадайте число от 1 до 100!")
 while True:
-guess = int(input("Ваш вариант: "))
+guess = (input("Ваш вариант: "))
 if guess == number:
 print("Поздравляем, вы угадали!")
 break
